06ae70affcf96f4031a9c69d9511820e74958f19
MctomSpdo <mctom.spdo@gmail.com>
2022-02-20 21:03:14 +0100

    init config file

--- API/user/create-user.php
+++ API/user/create-user.php
@@ -1,9 +1,6 @@
 <?php
 
-$_db_host = "localhost";
-$_db_datenbank = "ssprojekt22";
-$_db_username = "server";
-$_db_passwort = "abcdefgh";
+$config = json_decode(file_get_contents('../../files/config.json'));
 
 //https://stackoverflow.com/questions/8945879/how-to-get-body-of-a-post-in-php
 $data = json_decode(file_get_contents('php://input'));
