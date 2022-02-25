3e9bd394662d8631bec7607c4eab7e73e5e9803d
MctomSpdo <mctom.spdo@gmail.com>
2022-02-25 21:31:54 +0100

    doc updates

--- dev/create-tables.sql
+++ dev/create-tables.sql
@@ -1,6 +1,4 @@
-/* User
-CREATE TABLE `ssprojekt22`.`user` ( `UserID` INT NOT NULL AUTO_INCREMENT ,  `username` VARCHAR(30) NOT NULL ,  `description` VARCHAR(300) NOT NULL ,  `gender` VARCHAR(1) NOT NULL ,  `profilePic` VARCHAR(30) NOT NULL ,  `createdOn` DATETIME NOT NULL ,  `phoneNumber` VARCHAR(15) NOT NULL ,  `email` VARCHAR(254) NOT NULL ,  `password` VARCHAR(32) NOT NULL ,  `deleted` TINYINT NOT NULL ,  `lastLogin` DATETIME,  `lastTriedLogin` DATETIME,  `permission` INT NOT NULL ,    PRIMARY KEY  (`ID`),    UNIQUE  (`username`),    UNIQUE  (`email`)) ENGINE = InnoDB;
-*/
+/* User */
 DROP TABLE IF EXISTS `user`;
 create table user
 (
@@ -24,9 +22,7 @@ create table user
         unique (username)
 );
 
-/* Permissions
-CREATE TABLE `ssprojekt22`.`permission` ( `PermID` INT NOT NULL AUTO_INCREMENT ,  `name` VARCHAR(30) NOT NULL ,  `isBlocked` BOOLEAN NOT NULL ,  `isAdmin` BOOLEAN NOT NULL ,  `canPost` BOOLEAN NOT NULL ,  `canLike` BOOLEAN NOT NULL ,  `canComment` BOOLEAN NOT NULL ,    PRIMARY KEY  (`ID`),    UNIQUE  (`name`)) ENGINE = InnoDB; 
-*/
+/* Permissions */
 DROP TABLE IF EXISTS `permission`;
 create table permission
 (
@@ -47,9 +43,7 @@ INSERT INTO permission (name, isBlocked, isAdmin, canPost, canLike, canComment) 
 INSERT INTO permission (name, isBlocked, isAdmin, canPost, canLike, canComment) VALUES ('User', 0, 0, 1, 1, 1);
 INSERT INTO permission (name, isBlocked, isAdmin, canPost, canLike, canComment) VALUES ('Blocked', 1, 0, 0, 0, 0);
 
-/* Category 
-CREATE TABLE `ssprojekt22`.`permission` ( `ID` INT NOT NULL AUTO_INCREMENT ,  `name` VARCHAR(30) NOT NULL ,  `isBlocked` BOOLEAN NOT NULL ,  `isAdmin` BOOLEAN NOT NULL ,  `canPost` BOOLEAN NOT NULL ,  `canLike` BOOLEAN NOT NULL ,  `canComment` BOOLEAN NOT NULL ,    PRIMARY KEY  (`ID`),    UNIQUE  (`name`)) ENGINE = InnoDB;
-*/
+/* Category */
 DROP TABLE IF EXISTS `category`;
 create table category
 (
@@ -61,9 +55,7 @@ create table category
         unique (name)
 );
 
-/* Post 
-CREATE TABLE `ssprojekt22`.`post` ( `PostID` INT NOT NULL AUTO_INCREMENT ,  `ImgPath` VARCHAR(60) NOT NULL ,  `Title` VARCHAR(30) NOT NULL ,  `Description` VARCHAR(2000) NOT NULL ,  `PostedOn` DATETIME NOT NULL ,  `FromUser` INT NOT NULL ,  `IsDeleted` TINYINT NOT NULL ,    PRIMARY KEY  (`PostID`)) ENGINE = InnoDB;
-*/
+/* Post */
 DROP TABLE IF EXISTS `post`;
 create table post
 (
@@ -77,9 +69,7 @@ create table post
     IsDeleted   tinyint       not null
 );
 
-/* Post_Category
-CREATE TABLE `ssprojekt22`.`post_category` ( `PoCaID` INT NOT NULL AUTO_INCREMENT ,  `Post` INT NOT NULL ,  `Category` INT NOT NULL ,    PRIMARY KEY  (`PoCaID`)) ENGINE = InnoDB;
-*/
+/* Post_Category */
 DROP TABLE IF EXISTS `post_category`;
 create table post_category
 (
@@ -89,9 +79,7 @@ create table post_category
     Category int not null
 );
 
-/* Comment 
-CREATE TABLE `ssprojekt22`.`comment` ( `CommentID` INT NOT NULL AUTO_INCREMENT ,  `Content` VARCHAR(300) NOT NULL ,  `Owner` INT NOT NULL ,  `Post` INT NOT NULL ,  `isDeleted` TINYINT NOT NULL ,    PRIMARY KEY  (`CommentID`)) ENGINE = InnoDB;
-*/
+/* Comment */
 DROP TABLE IF EXISTS `comment`;
 create table comment
 (
@@ -103,9 +91,7 @@ create table comment
     isDeleted tinyint      not null
 );
 
-/* PostLiked
-CREATE TABLE `ssprojekt22`.`postliked` ( `PostLikedID` INT NOT NULL AUTO_INCREMENT ,  `Post` INT NOT NULL ,  `User` INT NOT NULL ,    PRIMARY KEY  (`PostLikedID`)) ENGINE = InnoDB;
-*/
+/* PostLiked */
 DROP TABLE IF EXISTS `postliked`;
 create table postliked
 (
@@ -115,9 +101,7 @@ create table postliked
     User        int not null
 );
 
-/* Follow
-CREATE TABLE `ssprojekt22`.`follow` ( `FollowID` INT NULL ,  `owner` INT NOT NULL ,  `Follows` INT NOT NULL ) ENGINE = InnoDB;
-*/
+/* Follow */
 DROP TABLE IF EXISTS `follow`;
 create table follow
 (
@@ -126,9 +110,7 @@ create table follow
     Follows  int not null
 );
 
-/* Token
-CREATE TABLE `ssprojekt22`.`token` ( `TokenID` INT NOT NULL AUTO_INCREMENT ,  `Token` VARCHAR(254) NOT NULL ,  `Owner` INT NOT NULL ,  `Created` DATETIME NOT NULL ,  `ValidUntil` DATETIME NOT NULL ,    PRIMARY KEY  (`TokenID`)) ENGINE = InnoDB;
-*/
+/* Token */
 DROP TABLE IF EXISTS `token`;
 create table token
 (
