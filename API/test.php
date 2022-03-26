a636a018869a9de739a787049c53a3aa74110065
Mctom_Spdo <mctom.spdo@gmail.com>
2022-03-26 19:13:28 +0100

    add database to test site

--- API/test.php
+++ API/test.php
@@ -8,8 +8,17 @@ var_dump($config);
 
 echo "<hr>";
 
-echo "<h1>Config</h1>";
+//database:
 
+echo "<h1>Database</h1>";
+
+$db = new mysqli($config->database->host, $config->database->username, $config->database->password, $config->database->database);
+
+if($db->connect_error) {
+    echo "Database connection failed!";
+} else {
+    var_dump($db);
+}
 
 
 ?>
