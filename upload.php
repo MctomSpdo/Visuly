1191907c1d730de30b384d82e91c13bdc2c3c3ea
Mctom_Spdo <mctom.spdo@gmail.com>
2022-04-29 22:49:40 +0200

    add #11

--- upload.php
+++ upload.php
@@ -50,7 +50,7 @@ $user->DBLoadFromUserID($userId, $db);
     <?php
     $navActive = 'upload';
     include "assets/nav.php" ?>
-    <div>
+    <div id="content">
         <div id="upload-header">
             <h2>Create a post</h2>
         </div>
