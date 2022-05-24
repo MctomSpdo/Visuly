e772d717faad756c5a4b6d9d561849eb40685f41
MctomSpdo <mctom.spdo@gmail.com>
2022-05-24 09:31:09 +0200

    reset password notice page

---
+++ reset-password.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="keywords" content="visuly, social Media, login">
+    <meta name="description" content="Login - Visuly">
+    <meta name="author" content="MctomSpdo">
+    <title>Login - Visuly</title>
+
+    <script src="./files/js/login.js" defer></script>
+
+    <link rel="stylesheet" href="./files/css/main.css">
+    <link rel="stylesheet" href="./files/css/login.css">
+    <link rel="icon" type="image/x-icon" href="./files/img/icon.svg">
+</head>
+<body>
+<div id="login-wrapper">
+    <div id="login-logo">
+        <h1>VISULY</h1>
+    </div>
+    <div id="login-box">
+        <h2>Reset password</h2>
+        <p>Sorry, this feature is not available yet</p>
+
+        <a href="login.php">Login</a>
+        <a href="signup.php">Create Account</a>
+    </div>
+</div>
+
+<div id="login-backdrop">
+    <ul class="box-area">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+</div>
+</body>
+</html>
